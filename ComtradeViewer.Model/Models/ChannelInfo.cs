89d9f138d93555421e1dfc74bc3eaa80498c5402
ari.ira17-mcb <ari.ira17@yandex.ru>
2026-06-24 14:07:42 +0600

    feat: implement ComtradeParser for ASCII .cfg and .dat files

---
+++ ComtradeViewer.Model/Models/ChannelInfo.cs
@@ -0,0 +1,11 @@
+namespace ComtradeViewer.Model.Models
+{
+    public class ChannelInfo
+    {
+        public int Index { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Unit { get; set; } = string.Empty;
+        public double FactorA { get; set; } 
+        public double FactorB { get; set; } 
+    }
+}
