50149eae70213db963b1fea501e0eb5f948681b2
ari.ira17-mcb <ari.ira17@yandex.ru>
2026-06-25 12:59:37 +0600

    test: add test

--- ComtradeViewer.Tests/ViewModelTests.cs
+++ ComtradeViewer.Tests/ViewModelTests.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Xunit;
 using Moq;
 using ComtradeViewer.ViewModel.ViewModels;
+using ComtradeViewer.ViewModel;
 using ComtradeViewer.Model.Services;
 using ComtradeViewer.Model.Models;
 
@@ -50,6 +51,119 @@ namespace ComtradeViewer.Tests
             viewModel.ChartWidth = 20;
 
             Assert.Equal(40, viewModel.ChartPoints.Count);
+        }
+
+        [Fact]
+        public void OpenFile_WhenNoChannels_ClearsSelection()
+        {
+            var mockParser = new Mock<IComtradeParser>();
+
+            mockParser
+                .Setup(p => p.Parse(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(new Dictionary<string, List<SamplePoint>>());
+
+            var viewModel = new MainViewModel(mockParser.Object);
+
+            viewModel.OpenFileCommand.Execute(new[] { "a", "b" });
+
+            Assert.Empty(viewModel.Channels);
+            Assert.Null(viewModel.SelectedChannel);
+            Assert.Empty(viewModel.ChartPoints);
+        }
+
+        [Fact]
+        public void OpenFile_IgnoresInvalidParameter()
+        {
+            var mockParser = new Mock<IComtradeParser>();
+
+            var viewModel = new MainViewModel(mockParser.Object);
+
+            viewModel.OpenFileCommand.Execute("wrong parameter");
+
+            mockParser.Verify(
+                p => p.Parse(It.IsAny<string>(), It.IsAny<string>()),
+                Times.Never);
+        }
+
+        [Fact]
+        public void OpenFile_WhenParserThrows_DoesNotThrow()
+        {
+            var mockParser = new Mock<IComtradeParser>();
+
+            mockParser
+                .Setup(p => p.Parse(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws(new Exception("Parse error"));
+
+            var viewModel = new MainViewModel(mockParser.Object);
+
+            var ex = Record.Exception(() =>
+                viewModel.OpenFileCommand.Execute(new[] { "a", "b" }));
+
+            Assert.Null(ex);
+        }
+
+        [Fact]
+        public void ChartWidth_LessThanOrEqual10_IsIgnored()
+        {
+            var viewModel = new MainViewModel();
+
+            double originalWidth = viewModel.ChartWidth;
+
+            viewModel.ChartWidth = 10;
+
+            Assert.Equal(originalWidth, viewModel.ChartWidth);
+        }
+
+        [Fact]
+        public void RelayCommand_CanExecute_ReturnsTrue_WhenPredicateIsNull()
+        {
+            var command = new RelayCommand(_ => { });
+
+            Assert.True(command.CanExecute(null));
+        }
+
+        [Fact]
+        public void RelayCommand_CanExecute_UsesPredicate()
+        {
+            var command = new RelayCommand(
+                _ => { },
+                _ => false);
+
+            Assert.False(command.CanExecute(null));
+        }
+
+        [Fact]
+        public void RelayCommand_Execute_CallsDelegate()
+        {
+            bool executed = false;
+
+            var command = new RelayCommand(
+                _ => executed = true);
+
+            command.Execute(null);
+
+            Assert.True(executed);
+        }
+
+        [Fact]
+        public void RelayCommand_RaiseCanExecuteChanged_RaisesEvent()
+        {
+            var command = new RelayCommand(_ => { });
+
+            bool raised = false;
+
+            command.CanExecuteChanged += (_, __) => raised = true;
+
+            command.RaiseCanExecuteChanged();
+
+            Assert.True(raised);
+        }
+
+        [Fact]
+        public void RelayCommand_Constructor_NullExecute_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new RelayCommand(null));
         }
     }
 }
